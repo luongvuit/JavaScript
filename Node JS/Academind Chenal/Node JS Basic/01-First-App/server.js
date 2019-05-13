50ec4f852157f6b1b1111ee8ce87472a6a69c9c4
LUONG VU <luongvu.work@gmail.com>
2019-05-13 15:40:23 +0700

    Commit
    Commit

--- Node JS/Academind Chenal/Node JS Basic/01-First-App/server.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-
-function onRequest(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write("Hello, Welcome to Coder's House Groups");
-    response.end();
-}
-
-http.createServer(onRequest).listen(90);
